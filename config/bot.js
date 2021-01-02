5238ca0fb5ddea986c69995477f2c200b0fe2fbc
Glitch (able-prickle-manchego) <none>
2021-01-02 15:15:27 +0000

    OK

---
+++ config/bot.js
@@ -0,0 +1,16 @@
+module.exports = {
+    emojis: {
+        off: ' <a:xo_cross:788297048058888243>',
+        error: ' <a:fizz_Warning_2:785485976159780905>',
+        queue: ':bar_chart:',
+        music: '<a:WumpusMusic:793705158596493312>', 
+        success: ' <a:TICK:785706368073072722>'
+    },
+
+    discord: {
+        prefix: '+',
+        activity: '+help / Happy New Year 2021'
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
